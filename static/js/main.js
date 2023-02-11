e5a811a5df893b7fbfb96cb7b55ce1af9b4d7500
ssatosays <ssatosays@gmail.com>
2023-02-11 17:46:53 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,27 @@
-window.onload = (event) => {
-  console.log('page is fully loaded');
+function updateBackgroundColor(target_id, rm_cls, add_cls) {
+  document.getElementById(String(target_id)).classList.remove(rm_cls);
+  document.getElementById(String(target_id)).classList.add(add_cls);
+}
+
+window.onload = () => {
+  var flg = true;
+  var first = 1;
+
+  setInterval(() => {
+    if (flg) {
+      updateBackgroundColor(first, 'lightyellow', 'red');
+      flg = false;
+    } else {
+      updateBackgroundColor(first, 'red', 'lightyellow');
+      // eslint-disable-next-line no-undef
+      $.ajax({
+        url: '/get_next_direction',
+        type: 'POST',
+        data: {'first': first}
+      }).done(function (data) {
+        first = Number(data.first);
+        flg = true;
+      });
+    }
+  }, 500);
 };
