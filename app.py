ebfd1cdd392115e3114863423d1472de9959bfca
ssatosays <ssatosays@gmail.com>
2023-02-12 22:29:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,78 +17,76 @@ def favicon():
         os.path.join(app.root_path, 'static/image'), 'favicon.png')
 
 
-@app.route('/get_next_direction_with_no_across', methods=['POST'])
-def get_next_direction_with_no_across():
-    """
-    """
+@app.route('/get_next_positions_with_no_across', methods=['POST'])
+def get_next_positions_with_no_across():
 
-    def _inner(position):  # noqa
-        return
-
-    request_dict = request.form.to_dict()
-    first = int(request_dict['first'])
-
-    if first == 1:
-        direction = random.randint(0, 2)
-        first = 2 if direction == 0 else 21
-
-    elif 1 < first < 20:
-        direction = random.randint(0, 3)
-        if direction == 0:
-            first -= 1
-        elif direction == 1:
-            first += 1
-        else:
-            first += 20
-
-    elif first % 20 == 0:
-        if first == 20:
+    def _inner(first):
+        if first == 1:
             direction = random.randint(0, 2)
-            first = 19 if direction == 0 else 40
-        elif first == 400:
-            direction = random.randint(0, 2)
-            first = 399 if direction == 0 else 380
-        else:
+            first = 2 if direction == 0 else 21
+
+        elif 1 < first < 20:
+            direction = random.randint(0, 3)
+            if direction == 0:
+                first -= 1
+            elif direction == 1:
+                first += 1
+            else:
+                first += 20
+
+        elif first % 20 == 0:
+            if first == 20:
+                direction = random.randint(0, 2)
+                first = 19 if direction == 0 else 40
+            elif first == 400:
+                direction = random.randint(0, 2)
+                first = 399 if direction == 0 else 380
+            else:
+                direction = random.randint(0, 3)
+                if direction == 0:
+                    first -= 1
+                elif direction == 1:
+                    first -= 20
+                else:
+                    first += 20
+
+        elif first % 20 == 1:
+            if first == 381:
+                direction = random.randint(0, 2)
+                first = 361 if direction == 0 else 382
+            else:
+                direction = random.randint(0, 3)
+                if direction == 0:
+                    first -= 20
+                elif direction == 1:
+                    first += 1
+                else:
+                    first += 20
+
+        elif 381 < first < 400:
             direction = random.randint(0, 3)
             if direction == 0:
                 first -= 1
             elif direction == 1:
                 first -= 20
             else:
-                first += 20
+                first += 1
 
-    elif first % 20 == 1:
-        if first == 381:
-            direction = random.randint(0, 2)
-            first = 361 if direction == 0 else 382
         else:
-            direction = random.randint(0, 3)
+            direction = random.randint(0, 4)
             if direction == 0:
-                first -= 20
+                first -= 1
             elif direction == 1:
+                first -= 20
+            elif direction == 2:
                 first += 1
             else:
                 first += 20
 
-    elif 381 < first < 400:
-        direction = random.randint(0, 3)
-        if direction == 0:
-            first -= 1
-        elif direction == 1:
-            first -= 20
-        else:
-            first += 1
+        return first
 
-    else:
-        direction = random.randint(0, 4)
-        if direction == 0:
-            first -= 1
-        elif direction == 1:
-            first -= 20
-        elif direction == 2:
-            first += 1
-        else:
-            first += 20
+    request_dict = request.form.to_dict()
+    first = _inner(int(request_dict['first']))
 
     return jsonify({'first': first})
 
